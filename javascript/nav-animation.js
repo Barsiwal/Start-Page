46311f1197deb8d0046e500624a60da6b2b671ee
Karan Barsiwal <Karan13047@iiitd.ac.in>
2014-10-03 15:13:29 +0530

    changed the content type to slide

--- javascript/nav-animation.js
+++ javascript/nav-animation.js
@@ -1,27 +1,22 @@
-$("document").ready(function(){
-	var mainnav=$('.main-nav');
-	$('.l2').hover(function(){
-		if(mainnav.hasClass('move2')){
+$("document").ready(function () {
+	var mainnav = $('.container');
+	
+	$('.l2 a').click(function () {
+		if (mainnav.hasClass('move2')) {
 			mainnav.removeClass('move2');
 		}
-		$('.main-nav').addClass('move1');
-		
-	},function(){
-			
-		});
-	$('.l3').hover(function(){
-		$('.main-nav').addClass('move2');
-	},function(){
-			
-		});
-	$('.l1').hover(function(){
-		if(mainnav.hasClass('move2') || mainnav.hasClass('move1')){
+		mainnav.addClass('move1');
+
+	});
+	$('.l3 a').click(function () {
+		mainnav.addClass('move2');
+	});
+	$('.l1 a').click(function () {
+		if (mainnav.hasClass('move2') || mainnav.hasClass('move1')) {
 			mainnav.removeClass('move2');
 			mainnav.removeClass('move1');
 		}
-		
-	},function(){
-			
-		});
-	
+
+	});
+
 });
